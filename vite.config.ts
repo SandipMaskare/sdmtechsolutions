991d6c2c6d8611dd62b50c6c3633cdaf30f1d5da
SandipMaskare <146016641+SandipMaskare@users.noreply.github.com>
2026-02-13 13:08:43 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,8 @@ import react from '@vitejs/plugin-react-swc';
 import path from 'path';
 
 // Import your custom tagger (use .js extension for ESM)
-import { generateTags } from './src/utils/myTagger.js';
+import { generateTags } from './src/utils/myTagger.ts';
+
 
 // Example usage during build (optional)
 console.log('Example tags:', generateTags('Welcome to SDM Tech Solution! Custom build tags'));
